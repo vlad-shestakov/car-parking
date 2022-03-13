4af1b09b1b782d086b40a74185985c5fbfd06fd4
Vladimir Shestakov <vlad-shestakov@mail.ru>
2022-03-14 02:47:25 +0500

    add parkspace business entity

--- src/main/java/com/shestakov/parkservice/interactor/model/Parkspace.java
+++ src/main/java/com/shestakov/parkservice/interactor/model/Parkspace.java
@@ -1,2 +1,15 @@
-package com.shestakov.parkservice.interactor.model;public class Parkspace {
+package com.shestakov.parkservice.interactor.model;
+
+import com.shestakov.parkservice.database.model.ParkspaceEntity;
+import lombok.Value;
+
+@Value
+public class Parkspace {
+
+    private int clientid;
+    private int parkspacenum;
+
+    public static Parkspace of(final ParkspaceEntity entity) {
+        return new Parkspace(entity.getClientid(), entity.getParkspacenum());
+    }
 }
